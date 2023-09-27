80bdbada3e4cf14e48051f3e77d9705f6b62f426
Kairat Sultan <garri.sultonov@mail.ru>
2023-09-27 20:19:46 +0600

    change beat worker flower

--- api/core/celery_app.py
+++ api/core/celery_app.py
@@ -1,12 +1,11 @@
 import os
 
 from celery import Celery
-from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.core.settings")
 
-app = Celery("drm")
+app = Celery("idealdent")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
